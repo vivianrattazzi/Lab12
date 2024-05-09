ce4d41a843868c2cbc64fbfb0a5fc2f7a9cbc533
FranPi14 <fra.pisty@gmail.com>
2024-05-09 18:55:03 +0200

    add stuff

--- UI/view.py
+++ UI/view.py
@@ -8,38 +8,66 @@ class View(ft.UserControl):
         self._page = page
         self._page.title = "Template application using MVC and DAO"
         self._page.horizontal_alignment = 'CENTER'
-        self._page.theme_mode = ft.ThemeMode.DARK
+        self._page.theme_mode = ft.ThemeMode.LIGHT
         # controller (it is not initialized. Must be initialized in the main, after the controller is created)
         self._controller = None
         # graphical elements
         self._title = None
-        self.txt_name = None
-        self.btn_hello = None
+
+        self.ddyear = None
+        self.ddcountry = None
+        self.txtN = None
+
+        self.btn_graph = None
+        self.btn_volume = None
+        self.btn_path = None
+
         self.txt_result = None
+        self.txtOut2 = None
+        self.txtOut3 = None
+
         self.txt_container = None
 
     def load_interface(self):
         # title
-        self._title = ft.Text("Hello World", color="blue", size=24)
+        self._title = ft.Text("TdP 2024 - Lab12: Prova tema d'esame", color="blue", size=24)
         self._page.controls.append(self._title)
 
         #ROW with some controls
-        # text field for the name
-        self.txt_name = ft.TextField(
-            label="name",
-            width=200,
-            hint_text="Insert a your name"
-        )
+        self.ddyear = ft.Dropdown(label="Anno")
+        self.ddcountry= ft.Dropdown(label="Nazione")
 
-        # button for the "hello" reply
-        self.btn_hello = ft.ElevatedButton(text="Hello", on_click=self._controller.handle_hello)
-        row1 = ft.Row([self.txt_name, self.btn_hello],
+        self.btn_graph = ft.ElevatedButton(text="Crea Grafo", on_click=self._controller.handle_graph)
+
+        row1 = ft.Row([self.ddyear, self.ddcountry, self.btn_graph],
                       alignment=ft.MainAxisAlignment.CENTER)
         self._page.controls.append(row1)
+        self._controller.fillDD()
 
         # List View where the reply is printed
-        self.txt_result = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self.txt_result = ft.ListView(expand=0, spacing=5, padding=5, auto_scroll=True)
         self._page.controls.append(self.txt_result)
+        self._page.update()
+
+
+        self.btn_volume = ft.ElevatedButton(text="Calcola Volumi", on_click=self._controller.handle_volume)
+        row2 = ft.Row([self.btn_volume],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row2)
+
+        self.txtOut2 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self._page.controls.append(self.txtOut2)
+        self._page.update()
+
+        self.txtN = ft.TextField(label="Lunghezza percorso")
+        self.btn_path = ft.ElevatedButton(text="Calcola percorso", on_click=self._controller.handle_path)
+
+        row3 = ft.Row([self.txtN, self.btn_path],
+                      alignment=ft.MainAxisAlignment.CENTER)
+        self._page.controls.append(row3)
+
+        self.txtOut3 = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self._page.controls.append(self.txtOut3)
         self._page.update()
 
     @property
